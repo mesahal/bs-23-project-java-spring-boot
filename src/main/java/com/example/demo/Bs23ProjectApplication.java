dfa42247d9bc6628da6644d5575c6fda1d7a42d1
sahal2000 <me.sahal2000@gmail.com>
2024-01-30 22:32:50 +0600

    initialized project

---
+++ src/main/java/com/example/demo/Bs23ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bs23ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bs23ProjectApplication.class, args);
+	}
+
+}
